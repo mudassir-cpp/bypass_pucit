4594ea3ae8f9e709bba216985653293835fe496a
Mudassir <gem920se@gmail.com>
2026-04-22 13:42:09 +0500

    FIRST COMMIT: Hatch setup

---
+++ src/bypass_pucit/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2026-present U.N. Owen <gem920se@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/bypass_pucit/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2026-present U.N. Owen <gem920se@gmail.com>
+#
+# SPDX-License-Identifier: MIT
