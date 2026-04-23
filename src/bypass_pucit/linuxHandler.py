ef6227fc7c8f2d51156d5f28dab8d99e24a63e33
Mudassir <gem920se@gmail.com>
2026-04-23 10:09:04 +0500

    ADD: SHELL idn

--- src/bypass_pucit/linuxHandler.py
+++ src/bypass_pucit/linuxHandler.py
@@ -2,10 +2,28 @@ import platform
 import  subprocess
 import  logging
 from typing import List
+import  os
+import  presentation as pr
+
+from samba.dcerpc.dcerpc import response
+
+from ColorDTO import  Color as cl
+
+
 def setForLinux():
+    # set for http and https
+    # 1  update bashrc or zshrc file
+    SHELL:str=getEnvVar("SHELL")
+    print(cl.RED+SHELL+cl.RESET)
+    if SHELL=="NONE":
+        pr.showError("NO SHELL configured or is'nt at default location")
+        return
+    SHELL=SHELL.split('/')[-1]
+
+    if SHELL=='zsh':
+        # get zsh file
 
     pass
-
 
 def runSubProcessWithStream(ls:List[str]):
     process=subprocess.Popen(
@@ -13,7 +31,15 @@ def runSubProcessWithStream(ls:List[str]):
         stdout=subprocess.PIPE, # stream the output to PIPE like |
         text=True # decode the bytes and Txt
     )
+    print(str(process.stdout))
+    response:str =""
     for ln in process.stdout:
+        response += ln+'\n'
         print(ln.strip()) # print the output stream
+    return response
 
-    return process.returncode
+def getEnvVar(var:str)->str:
+    res=os.environ.get("SHELL")
+    if res is None:
+        return "NONE"
+    return res
--- src/bypass_pucit/presentation.py
+++ src/bypass_pucit/presentation.py
@@ -1,4 +1,14 @@
-
-
+from ColorDTO import Color as cl
 def showHelpPage():
     print("Will print help page")
+
+
+def httpFailure(whatNotFound:str):
+    print(cl.RED+f"{whatNotFound} SHELL is NOT SUPPORTED so can't set http|https proxy ")
+    print("All tools depending on http|https methods wouldn't access internet i.e pub.dev")
+    print(cl.RESET)
+
+
+def showError(errStr:str):
+    print(cl.RED+errStr+cl.RESET)
+    print(cl.RESET)
